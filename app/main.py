5ed6847bd8a32236b624d327eb2d88ac3d5360fc
Henry Davies <Henry Davies>
2023-02-08 09:07:17 +0000

    Add basic parsing of questions and answers. Needs parsing error handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+import re
+from fastapi import FastAPI, Depends
 
 from app.prompt import send_prompt
+from app.settings import get_settings, Settings
 
 app = FastAPI()
 
@@ -9,5 +11,12 @@ def health_check():
     return {"status": "OK"}
 
 @app.post("/generate")
-def generate_quiz(question_count: int = 10):
-    return send_prompt(f"Write a pub quiz question. Delimit the question and answer with a newline character.")
+def generate_quiz(theme: str, question_count: int = 10, settings: Settings = Depends(get_settings)):
+    prompt_response = send_prompt(f"Write a {theme}-themed pub quiz round with {question_count} questions and answers.", settings)
+    prompt_text = prompt_response.choices[0].text
+    # prompt_text = "\n\n1. Who was the first president of the United States? \nAnswer: George Washington\n\n2. What year did the American Revolutionary War end? \nAnswer: 1783\n\n3. Who wrote the Declaration of Independence? \nAnswer: Thomas Jefferson\n\n4. What country did the United States gain independence from? \nAnswer: Great Britain\n\n5. What year did World War II end? \nAnswer: 1945\n\n6. Who was the leader of Nazi Germany during World War II? \nAnswer: Adolf Hitler\n\n7. What year did the Berlin Wall fall? \nAnswer: 1989\n\n8. Who was the first Prime Minister of India? \nAnswer: Jawaharlal Nehru\n\n9. What year did the French Revolution begin? \nAnswer: 1789\n\n10. Who was the first Emperor of China? \nAnswer: Qin Shi Huang"
+    questions = [{"question": re.sub(r'[\d]+\.\s+', r'', q), "answer": a.replace("Answer: ", "").strip()} for q, a in (i.split("\n") for i in prompt_text.split("\n\n") if i)]
+    return {
+        "total": len(questions),
+        "items": questions
+    }
--- app/prompt.py
+++ app/prompt.py
@@ -1,15 +1,13 @@
 import os
 import openai
-from fastapi import Depends
-from app.settings import get_settings, Settings
 
-def send_prompt(prompt: str, settings: Settings = Depends(get_settings)):
+def send_prompt(prompt: str, settings):
     openai.api_key = settings.openai_api_key
     return openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
         temperature=0.3,
-        max_tokens=25,
+        max_tokens=400,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0
